9f6fd0b7bf42046fb2f98272e2666a924a6d42c1
F3GR <egmver.2@gmail.com>
2023-07-12 22:32:09 +0400

    finished base layout

--- src/components/page.js
+++ src/components/page.js
@@ -3,12 +3,25 @@ import { checkIfCurrent, removeCurrentStatus, createElementWithAttributes } from
 const content = document.querySelector('.content');
 
 const header = createElementWithAttributes('header', {}, content);
+
+const asideIcon = createElementWithAttributes('div', {class: `aside-icon`}, header);
+
+const heading = createElementWithAttributes('div', {class: `heading`}, header);
+
 const logoIcon = createElementWithAttributes('img', { 
     src: '../src/originals/fav-icon.svg',
-    alt: `ToDo List logo`}, header);
+    class: `logo`,
+    alt: `ToDo List logo`}, heading);
 
-const heading = createElementWithAttributes('h1', {}, header);
-heading.textContent = `ToDo List`;
+const headingText = createElementWithAttributes('h1', {}, heading);
+headingText.textContent = `ToDo List`;
+
+const emptyDiv = createElementWithAttributes('div', {}, header);
+
+const viewOptionsIcon = createElementWithAttributes('img', { 
+    src: '../src/originals/view-options.svg',
+    class: `view-options`,
+    alt: `View Options logo`}, header);
 
 const sidebar = createElementWithAttributes('aside', {}, content);
 
@@ -96,16 +109,22 @@ mainHeadText.textContent = "All";
 
 const mainTaskMenu = createElementWithAttributes('div', {class: `task-menu`}, main);
 
-const buttonHighPriority = createElementWithAttributes('button', {class: `high-priority`}, mainTaskMenu);
+const mainTaskNumber = createElementWithAttributes('div', {class: `task-number`}, mainTaskMenu);
+const taskMenuText = createElementWithAttributes('span', {}, mainTaskNumber);
+taskMenuText.textContent = `Tasks (0)`;
+
+const buttonsTaskSubMenu = createElementWithAttributes('div', {class: `buttons-task-submenu`}, mainTaskMenu);
+
+const buttonHighPriority = createElementWithAttributes('button', {class: `high-priority`}, buttonsTaskSubMenu);
 buttonHighPriority.textContent = `High`;
 
-const buttonMediumPriority = createElementWithAttributes('button', {class: `medium-priority`}, mainTaskMenu);
+const buttonMediumPriority = createElementWithAttributes('button', {class: `medium-priority`}, buttonsTaskSubMenu);
 buttonMediumPriority.textContent = `Medium`;
 
-const buttonNormalPriority = createElementWithAttributes('button', {class: `normal-priority`}, mainTaskMenu);
+const buttonNormalPriority = createElementWithAttributes('button', {class: `normal-priority`}, buttonsTaskSubMenu);
 buttonNormalPriority.textContent = `Normal`;
 
-const selectSortOption = createElementWithAttributes('select', {name: `sorting-option`}, mainTaskMenu);
+const selectSortOption = createElementWithAttributes('select', {name: `sorting-option`}, buttonsTaskSubMenu);
 selectSortOption.textContent = `Sort by: `;
 
 const sortByDate = createElementWithAttributes('option', {value: `date-from-oldest`},selectSortOption);
@@ -117,8 +136,39 @@ sortByPriority.textContent = `Priority`;
 const sortByStatus = createElementWithAttributes('option', {value: `priority-from-due`},selectSortOption);
 sortByStatus.textContent = `Status`;
 
-const headTaskList = createElementWithAttributes('div', {class: `task-menu`}, main);
+const addNewProjectIcon = createElementWithAttributes('img', { 
+    src: '../src/originals/add-new.svg',
+    alt: `Add new project icon`
+}, mainTaskMenu);
 
-const mainTaskNumber = createElementWithAttributes('div', {class: `task-number`}, mainTaskMenu);
-const taskMenuText = createElementWithAttributes('span', {}, mainTaskNumber);
-taskMenuText.textContent = `Tasks (0)`;
+const taskList = createElementWithAttributes('ul', {class: `task-list`}, main);
+
+const exampleTask = createElementWithAttributes('li', {class: `task`, data: `0`}, taskList);
+
+const taskStatusIcon  = createElementWithAttributes('img', {
+    src: `../src/originals/radio-unchecked.svg`, 
+    alt: `Task status icon`
+}, exampleTask);
+
+const taskNameBox = createElementWithAttributes('div', {class: `task-name-box`}, exampleTask);
+const taskName = createElementWithAttributes('span', {class: `task-name-box`}, taskNameBox);
+taskName.textContent = `New task`;
+
+const taskDueDateBox = createElementWithAttributes('div', {class: `task-due-date`, }, exampleTask);
+const taskDueDateText = createElementWithAttributes('span', {class: ``, }, taskDueDateBox);
+taskDueDateText.textContent = `2023-01-07`;
+
+const taskEditIcon = createElementWithAttributes('img', {
+    src: `../src/originals/edit.svg`, 
+    alt: `Task edit information icon`
+}, exampleTask);
+
+const taskRemoveIcon = createElementWithAttributes('img', {
+    src: `../src/originals/delete.svg`, 
+    alt: `Task remove icon`
+}, exampleTask);
+
+const taskUnfoldIcon = createElementWithAttributes('img', {
+    src: `../src/originals/unfold.svg`, 
+    alt: `Task information unfold or fold icon`
+}, exampleTask);
